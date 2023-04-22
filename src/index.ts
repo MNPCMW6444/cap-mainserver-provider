129285209a6749f4889804e8533fd00cb785a88a
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-22 14:02:26 +0300

    ok

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { MainServerContext } from "./context/MainServerContext";
-import { ProvideMainServer } from "./providers/MainServerProvider";
-
-export { ProvideMainServer, MainServerContext };
